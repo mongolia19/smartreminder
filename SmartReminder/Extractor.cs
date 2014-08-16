2c5763e59f0d1967c01a8f5e6f030542e3e4826c
mongolia19 <mongolia19@gmail.com>
2014-08-16 22:02:37 +0800

    Add article extractor
    Add article extractor

---
+++ SmartReminder/Extractor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace SmartReminder
+{
+    class Extractor
+    {
+
+
+        public static ArrayList extract(String article)
+        {
+            ArrayList outputList = new ArrayList();
+           String[] sentenceArray =article.Split('.', '!', '?');
+
+           for (int i = 0; i < sentenceArray.GetLength(0); i++)
+           {
+               outputList.Add(sentenceArray[i]);
+           }
+
+           return outputList;
+        }
+
+
+        public static ArrayList GetTitles(String article) 
+        {
+            
+
+
+        
+        }
+    }
+}
