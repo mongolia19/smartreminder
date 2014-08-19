1e337cfbace7bcf4e031dc1e5e0f5d39b3938742
mongolia19 <mongolia19@gmail.com>
2014-08-19 21:46:52 +0800

    Add preprocessTool
    Signed-off-by: mongolia19 <mongolia19@gmail.com>

---
+++ SmartReminder/PreProcessTools.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartReminder
+{
+    class PreProcessTools
+    {
+
+        static String[] PunctuationList = { "?", "。", "，", "：", "“", "“", "”", "？", "," ,"\r","\n"," ","、","/"};
+
+
+        static String[] CNStopWords = { "我", "你", "的", "得", "这", "那", "他", "是", "为","在","了" ,"有","就","到","个","不","否"};
+        
+        public static String RemovePunctuation(String RawText)//Remove charactors such as , . ? 
+        {
+            for (int i = 0; i < PunctuationList.GetLength(0); i++)
+            {
+                if (RawText.Contains(PunctuationList[i]))
+                {
+                    RawText = RawText.Replace(PunctuationList[i],"");
+
+                }
+            }
+
+
+            return RawText;
+        
+        
+        }
+
+
+        public static String RemoveCNStopWords(String RawText)//Remove charactors such as , . ? 
+        {
+            for (int i = 0; i < CNStopWords.GetLength(0); i++)
+            {
+                if (RawText.Contains(CNStopWords[i]))
+                {
+                    RawText = RawText.Replace(CNStopWords[i], "");
+
+                }
+            }
+
+
+            return RawText;
+
+
+        }
+
+
+
+
+    }
+}
